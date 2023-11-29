1752a8a8ba6c680df1384214675c0386f96aed11
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2023-11-29 16:25:07 -0500

    add product details screen

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,23 +1,15 @@
 import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa'
 
 const Rating = ({value, text}) => {
+    const stars = [1, 2, 3, 4, 5]; /* 5 stars is max rating */
+
     return (
         <div className='rating'>
-            <span>
-                {value >= 1 ? <FaStar /> : value >= 0.5 ? <FaStarHalfAlt /> : <FaRegStar />}
-            </span>
-            <span>
-                {value >= 2 ? <FaStar /> : value >= 1.5 ? <FaStarHalfAlt /> : <FaRegStar />}
-            </span>
-            <span>
-                {value >= 3 ? <FaStar /> : value >= 2.5 ? <FaStarHalfAlt /> : <FaRegStar />}
-            </span>
-            <span>
-                {value >= 4 ? <FaStar /> : value >= 3.5 ? <FaStarHalfAlt /> : <FaRegStar />}
-            </span>
-            <span>
-                {value >= 5 ? <FaStar /> : value >= 4.5 ? <FaStarHalfAlt /> : <FaRegStar />}
-            </span>
+            {stars.map((star) => (
+                <span key={star}>
+                    {value >= star ? <FaStar /> : value >= star - 0.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+                </span>
+            ))}
             <span className="rating-text">{text && text}</span>
         </div>
     )
